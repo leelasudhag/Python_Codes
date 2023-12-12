822de473e5e78954054c78416105b9a74830c0b3
leelasudha <sudhagaddec@gmail.com>
2023-12-12 07:31:28 -0800

    changes made to nestedwhile make sre they are printing on sam eline

--- nestedwhile.py
+++ nestedwhile.py
@@ -1,8 +1,11 @@
 i = 1
-j = 1
+
 while i <= 5:
-    print("practice i")
-    i = i+1
+    print("practice i ",end="")
+    j = 1
     while j <= 5:
-        print("practice j")
+        print("practice j ",end="")
         j = j+1
+
+    i = i+1
+    print()
